6a0a497443f47f29c381db23984191602a560bb9
fede <federiusaki@gmail.com>
2022-07-07 23:36:12 -0500

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,52 @@
+import {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+        const [categories, setCategories] = useState(['World of warcraft'])
+
+        const onAddCategory = (newCategory) => {
+            //categories.push(onNewCategory);
+
+            if (categories.includes(newCategory) ) return;
+
+            //console.log(newCategory);
+            setCategories ([newCategory,...categories]);
+        }
+
+        return (
+            <>
+                {/* Titulo */}
+                <h1>Gif para los panas</h1>
+
+                {/* Input */}
+                <AddCategory 
+                    //setCategories= {setCategories} 
+                    onNewCategory = {onAddCategory}
+                
+                />
+
+                    {/* Listado de gif */}
+                    
+
+                   
+                {
+                        categories.map( (category) => (
+                                <GifGrid
+                                key={category}
+                                category={category} />
+                            ))
+                            
+                }
+                        
+                    
+
+
+                    {/* Gif Item */}
+                        
+                
+            
+            </>
+  )
+}
---
+++ src/helpers/getGif.js
@@ -0,0 +1,17 @@
+
+
+
+export const getGif = async (category) => {
+
+    const url=`https://api.giphy.com/v1/gifs/search?api_key=9gtn1qG6BhbYzIhn2KIEGQFp4wkCQGaz&q=${category}&limit=10`;
+    const resp = await fetch ( url);
+    const {data} = await resp.json ();
+    const gif = data.map (img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+
+    }))
+    return gif;
+
+}
